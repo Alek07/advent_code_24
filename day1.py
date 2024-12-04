b1116690f08410a2dc13d770019f87760473f094
Alek07 <alekissac@gmail.com>
2024-12-03 20:09:56 -0500

    aoc day 1 - part 2

--- day1.py
+++ day1.py
@@ -1,5 +1,14 @@
 from utils.file_utils import read_file
 
+def calculated_total_similarity_score(left, right):
+    similarity_score = 0
+
+    for value in left:
+        similarity_score += (int(value) * right.count(value))
+
+    return similarity_score
+
+
 def get_total_distance(locations_id):
     total_distance = 0
     left, right = zip(*(location.split() for location in locations_id))
@@ -10,7 +19,9 @@ def get_total_distance(locations_id):
     for l, r in zip(left, right):
         total_distance += abs(int(l) - int(r))
 
-    return total_distance
+    total_similarity_score = calculated_total_similarity_score(left, right)
+
+    return [total_distance, total_similarity_score]
 
 def main():
     locations = read_file('./input/day1.txt')
